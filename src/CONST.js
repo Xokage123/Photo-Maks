5f2398dbb1a2f07704779fbbace4b7a6d5c7cb9f
Maks <maxartem0419@gmail.com>
2021-05-01 18:24:40 +0300

    update

--- src/CONST.js
+++ src/CONST.js
@@ -1,7 +1,7 @@
 module.exports = {
     access_key: "df7ICabs2U1sK_fvyU2BuzlCLv-eduqU51fMxIn6Hvs",
     secret_key: "3cVeyZ3__ekbRdS5MuHkXN9yj4MFWCauLCVzE8hGhtA",
-    URI: 'https://photo-maks.vercel.app/photo',
+    URI: 'https://photo-maks.vercel.app/photos',
     response_type: "code",
     scope: "public+write_likes",
     // Для тестов
