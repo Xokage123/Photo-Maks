696657d1f5442e094a00fff600e5848506eda96f
Maks <maxartem0419@gmail.com>
2021-05-01 22:46:35 +0300

    oauth

--- src/connection/authorize.jsx
+++ src/connection/authorize.jsx
@@ -6,10 +6,7 @@ import options from "../CONST";
 const authorizationAddress = `https://unsplash.com/oauth/authorize?client_id=${options.access_key}&redirect_uri=${options.URI}&response_type=${options.response_type}&scope=${options.scope}`;
 
 export default function Authorize () {
-  useEffect(()=> {
-    window.location.assign(authorizationAddress)
-  },[])
-
+  window.location.assign(authorizationAddress);
   return (
     <div>Загрузка...</div>
   )
