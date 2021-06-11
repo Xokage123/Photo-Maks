058cb712d012f5e5023bdff3c272384c6e6fb53a
Maks <maxartem0419@gmail.com>
2021-06-11 18:07:51 +0300

    t

--- src/connection/token.js
+++ src/connection/token.js
@@ -2,6 +2,7 @@ export async function getTheToken(url) {
     const answer = await fetch(url, {
         method: 'POST'
     });
+    console.log(answer);
     const infoToken = await answer.json();
     if (!infoToken.error) {
         localStorage.setItem("access_token", infoToken.access_token);
